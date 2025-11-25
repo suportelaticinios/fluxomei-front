78897580150c2300e3fd8da4a0a5ef8fe45ad949
Moisés Moreira <moisessilva12337@gmail.com>
2025-11-25 15:55:44 -0300

    Correção

--- assets/js/relatorios.js
+++ assets/js/relatorios.js
@@ -5,34 +5,30 @@ const btnGerarRelatorioBalanco = document.getElementById('btnRelatorioBalanco');
 
 btnGerarRelatorioMensal.addEventListener('click', function ()
 {
-    let data = document.querySelector('#mes-mensal').value;
+    let data = document.querySelector('#mes-balanco').value;
     data = data.split('-');
     let ano = data[0];
     let mes = data[1];
 
-    // faz uma requisição a api para gerar relatório mensal
-    fetch(url + `mensal?mes=${mes}&ano=${ano}`, {
+    document.getElementById("loadingScreen").classList.remove("hidden");
+
+    fetch(url + `mensal?ano=${ano}&mes=${mes}`, {
         method: 'GET',
         headers: {
-            "Authorization": `Bearer ${token}` // token no header
-        },
-        body: JSON.stringify() // só enviar body em POST/PUT/PATCH
-    })
-    .then(response => {
-        if (!response.ok)
-        {
-            throw new Error(`Erro na requisição: ${response.status}`);
+            "Authorization": `Bearer ${token}`
         }
-        return response.json(); // converte o retorno para JSON
     })
-    .then(data => {
-        console.log("Sucesso: ", data);
-        document.querySelector("#previa-relatorios").classList.remove('hidden');
+    .then(response => response.blob())   // <<< PDF BINÁRIO AQUI
+    .then(blob => {
+        const fileURL = URL.createObjectURL(blob);
+        window.open(fileURL, "_blank");  // <<< ABRE O PDF
     })
     .catch(error => {
-        console.log("Error: ", error.message);
+        console.log("ERRO PDF:", error);
     })
-
+    .finally(() => {
+        document.getElementById("loadingScreen").classList.add("hidden");
+    });
 })
 
 btnGerarRelatorioAnual.addEventListener('click', function ()
@@ -41,6 +37,8 @@ btnGerarRelatorioAnual.addEventListener('click', function ()
     data = data.split('-');
     let ano = data[0];
     let mes = data[1];
+
+    document.getElementById("loadingScreen").classList.remove("hidden");
 
     fetch(url + `anual?ano=${ano}&mes=${mes}`, {
         method: 'GET',
@@ -55,14 +53,20 @@ btnGerarRelatorioAnual.addEventListener('click', function ()
     })
     .catch(error => {
         console.log("ERRO PDF:", error);
+    })
+    .finally(() => {
+        document.getElementById("loadingScreen").classList.add("hidden");
     });
 })
 
-btnGerarRelatorioBalanco.addEventListener('click', function () {
+btnGerarRelatorioBalanco.addEventListener('click', function () 
+{
     let data = document.querySelector('#mes-balanco').value;
     data = data.split('-');
     let ano = data[0];
     let mes = data[1];
+
+    document.getElementById("loadingScreen").classList.remove("hidden");
 
     fetch(url + `balanco?ano=${ano}&mes=${mes}`, {
         method: 'GET',
@@ -77,5 +81,8 @@ btnGerarRelatorioBalanco.addEventListener('click', function () {
     })
     .catch(error => {
         console.log("ERRO PDF:", error);
+    })
+    .finally(() => {
+        document.getElementById("loadingScreen").classList.add("hidden");
     });
 });
