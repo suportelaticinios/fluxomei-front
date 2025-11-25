18b43bc9bb795c9b21858199fad89bbd069ca8e2
Moisés Moreira <moisessilva12337@gmail.com>
2025-11-25 15:42:08 -0300

    Melhorias nas telas de gerar relatórios

--- assets/js/relatorios.js
+++ assets/js/relatorios.js
@@ -37,68 +37,26 @@ btnGerarRelatorioMensal.addEventListener('click', function ()
 
 btnGerarRelatorioAnual.addEventListener('click', function ()
 {
-    let data = document.querySelector('#ano-anual').value;
+    let data = document.querySelector('#mes-balanco').value;
     data = data.split('-');
     let ano = data[0];
     let mes = data[1];
 
-    // faz uma requisição a api para gerar relatório mensal
-    fetch(url +  `anual?ano=${ano}`, {
+    fetch(url + `anual?ano=${ano}&mes=${mes}`, {
         method: 'GET',
         headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}` // token no header
+            "Authorization": `Bearer ${token}`
         }
     })
-    .then(response => {
-        if (!response.ok)
-        {
-            throw new Error(`Erro na requisição: ${response.status}`);
-        }
-        return response.json(); // converte o retorno para JSON
-    })
-    .then(data => {
-        console.log("Sucesso: ", data);
-        document.querySelector("#previa-relatorios").classList.remove('hidden');
+    .then(response => response.blob())   // <<< PDF BINÁRIO AQUI
+    .then(blob => {
+        const fileURL = URL.createObjectURL(blob);
+        window.open(fileURL, "_blank");  // <<< ABRE O PDF
     })
     .catch(error => {
-        console.log("Error: ", error.message);
-    })
-
+        console.log("ERRO PDF:", error);
+    });
 })
-
-// btnGerarRelatorioBalanco.addEventListener('click', function ()
-// {
-//     let data = document.querySelector('#mes-balanco').value;
-//     console.log(data)
-//     data = data.split('-');
-//     let ano = data[0];
-//     let mes = data[1];
-
-//     // faz uma requisição a api para gerar relatório mensal
-//     fetch(url +  `balanco?ano=${ano}&mes=${mes}`, {
-//         method: 'GET',
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": `Bearer ${token}` // token no header
-//         }
-//     })
-//     .then(response => {
-//         if (!response.ok)
-//         {
-//             throw new Error(`Erro na requisição: ${response.status}`);
-//         }
-//         return response.json(); // converte o retorno para JSON
-//     })
-//     .then(data => {
-//         console.log("Sucesso: ", data);
-//         document.querySelector("#previa-relatorios").classList.remove('hidden');
-//     })
-//     .catch(error => {
-//         console.log("Error: ", error.message);
-//     })
-
-// })
 
 btnGerarRelatorioBalanco.addEventListener('click', function () {
     let data = document.querySelector('#mes-balanco').value;
